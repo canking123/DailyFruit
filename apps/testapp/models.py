71792066e62eb601940abfeb0408fa9ed9c4759c
NSaviour <wenjian.22@163.com>
2017-09-09 13:13:47 +0800

    修改gitignore,新建testapp应用

--- apps/testapp/models.py
+++ apps/testapp/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Development(models.Model):
-    nick_name = models.CharField(max_length=10)
-
-    class Meta:# 元信息类
-        db_table = 'development'# 指定表的名称
-
-    def __str__(self):
-        return self.btitle
+class HeroInfo(models.Model):
+    hname=models.CharField(max_length=20)
+    hcontent=models.CharField(max_length=100)
