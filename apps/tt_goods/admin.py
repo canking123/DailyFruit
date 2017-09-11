bc1435e446761d35b0ad8947d6f6db618c17969d
NSaviour <wenjian.22@163.com>
2017-09-11 17:33:38 +0800

    index、detail初稿

--- apps/tt_goods/admin.py
+++ apps/tt_goods/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import TypeInfo,GoodsInfo
 # Register your models here.
+
+class TypeInfoAdmin(admin.ModelAdmin):
+    list_display=['pk','ttitle','isDelete']
+
+class GoodsInfoAdmin(admin.ModelAdmin):
+    list_display=['pk','gtitle','gstorage','gclick','gtype','isDelete']
+
+admin.site.register(TypeInfo,TypeInfoAdmin)
+admin.site.register(GoodsInfo,GoodsInfoAdmin)
