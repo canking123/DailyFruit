7d44bf40460e0920a26b0e6b1aaf7295bca0cfc5
NSaviour <wenjian.22@163.com>
2017-09-11 21:38:58 +0800

    今日工作完成

--- apps/tt_goods/views.py
+++ apps/tt_goods/views.py
@@ -1,37 +1,41 @@
 from django.shortcuts import render
 from .models import *
+from tt_cart.models import *
 
 
 # Create your views here.
 def index(request):
-    # list = []
-    # for i in range(1, 7):
-    #     glist= GoodsInfo.objects.all().filter(gtype=i)
-    #     glist = glist[0:4]
-    #     list.append(glist)
-    #
-    # context = {'glist': list}
-    cont={}
-    cont2={}
-
+    cont = {}
+    cont2 = {}
     for i in range(1, 7):
+        cats = CartInfo.objects.all()
+        print(cats)
+        print('1234')
+
         glist = GoodsInfo.objects.filter(gtype=i)
         glist = glist[0:4]
         cont2 = {'glist%s' % (i): glist}
         cont.update(cont2)
-
     context = cont
     return render(request, 'tt_goods/index.html', context)
 
 
 def list(request, l_id):
-    glist = GoodsInfo.objects.all().filter(gtype_id=l_id)
-    context = {'glist': glist}
-    return render(request, 'tt_goods/list.html',context)
+    glist = GoodsInfo.objects.filter(gtype_id=l_id)
+    count = GoodsInfo.objects.count()
+
+    n_glist = GoodsInfo.objects.all()
+    n_glist = n_glist[count - 3:count]
+
+    context = {'glist': glist, 'n_glist': n_glist}
+    return render(request, 'tt_goods/list.html', context)
 
 
-def detail(request,g_id):
-    goods = GoodsInfo.objects.all().filter(id=g_id)
+def detail(request, g_id):
+    goods = GoodsInfo.objects.filter(id=g_id)
+    count = GoodsInfo.objects.count()
 
-    context={'goods':goods}
-    return render(request, 'tt_goods/detail.html',context)
+    n_glist = GoodsInfo.objects.all()
+    n_glist = n_glist[count - 3:count]
+    context = {'goods': goods, 'n_glist': n_glist}
+    return render(request, 'tt_goods/detail.html', context)
