0f521521a7c0ed232d43e82a8542db5ca0e1c74b
Damon0906 <444043012@qq.com>
2017-09-13 08:31:23 +0800

    9/13/8:30

--- apps/tt_cart/urls.py
+++ apps/tt_cart/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^sp/$', views.cart),
+    url(r'^add/$', views.add),
 ]
--- apps/tt_cart/views.py
+++ apps/tt_cart/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
-
+from .models import CartInfo
+from tt_goods.models import GoodsInfo
+from django.http import JsonResponse
 # Create your views here.
 
 
 def cart(request):
     return render(request, 'tt_cart/cart.html')
+
+
+def add(request):
+    dict = request.GET
+    gid = int(dict.get('gid', ''))
+    count = int(dict.get('count'))
+    print('gid' + gid)
+    kucun = GoodsInfo.objects.get(pk=gid).gkucun
+    uid = int(request.session.get('uid'))
+
+    carts = CartInfo.objects.filter(user_id=uid, goods_id=gid)
+    if len(carts) == 0:
+        cart = CartInfo()
+        cart.user_id = request.session.get('uid')
+        cart.goods_id = int(gid)
+        cart.count = count
+        cart.save()
+    else:
+        cart = carts[0]
+        count1 = cart.count+count
+        if count1 > kucun:
+            cart.count = kucun
+        else:
+            cart.count += count
+        cart.save()
+
+    return JsonResponse({'isok': 1})
