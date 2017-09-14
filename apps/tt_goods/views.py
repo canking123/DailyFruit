46d28fcf9bb650d5b6a96ddbd7c77a36e05f5b88
NSaviour <wenjian.22@163.com>
2017-09-14 18:28:37 +0800

    tt_goods1.0

--- apps/tt_goods/views.py
+++ apps/tt_goods/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from tt_cart.models import *
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -8,9 +9,6 @@ def index(request):
     cont = {}
     cont2 = {}
     for i in range(1, 7):
-        cats = CartInfo.objects.all()
-        print(cats)
-        print('1234')
         glist = GoodsInfo.objects.filter(gtype=i)
         glist = glist[0:4]
         cont2 = {'glist%s' % (i): glist}
@@ -19,7 +17,7 @@ def index(request):
     return render(request, 'tt_goods/index.html', context)
 
 
-def list(request, l_id, l_sort):
+def list(request, pindex,l_id, l_sort):
     if (l_id == '100'):
         glist = GoodsInfo.objects.all()  # 全部商品
     else:
@@ -31,15 +29,21 @@ def list(request, l_id, l_sort):
         glist = glist.order_by('gprice')
     elif (l_sort == '2'):
         glist = glist.order_by('gclick')
-    gsort = l_sort  # 排序方式
 
+    gsort = {'num': l_sort}# 排序方式
     gtype = TypeInfo.objects.filter(id=l_id)  # 一类商品的类型
 
     count = GoodsInfo.objects.count()  # 总数
     n_glist = GoodsInfo.objects.all()
     n_glist = n_glist[count - 3:count]  # 取出最新的三件商品
 
-    context = {'glist': glist, 'n_glist': n_glist, 'gtype': gtype, 'gsort': gsort}
+    p = Paginator(glist, 15)
+
+    pIndex={'page':pindex}#传入的页码
+    glist = p.page(int(pindex))
+    plist = p.page_range
+
+    context = {'glist': glist, 'n_glist': n_glist, 'gtype': gtype, 'gsort': gsort,'plist': plist, 'pIndex': pIndex}
     return render(request, 'tt_goods/list.html', context)
 
 
