575ec38665050817dbd1b5e4b81e94a4b4a851f3
Damon0906 <444043012@qq.com>
2017-09-19 19:53:45 +0800

    1.2

--- apps/tt_user/user_decorators.py
+++ apps/tt_user/user_decorators.py
@@ -1,16 +1,22 @@
 # -*- coding:utf-8 -*-
 from django.shortcuts import redirect
+from django.http import HttpRequest,JsonResponse
 
 
 def user_login(func):
     def call_func(request, *args, **kwargs):
         # 判断用户是否登录
-        if request.session.has_key('uid'):
+        if 'uid' in request.session:
             # 如果登录，则继续执行视图
             return func(request, *args, **kwargs)
+            print('uid')
         else:
             # 如果没有登录，则转到登录页
-            return redirect('/user/login/')
+            if request.is_ajax():
+                print('isajax')
+                return JsonResponse({'ok': 1})
+            else:
+                return redirect('/user/login/')
 
     return call_func
 
