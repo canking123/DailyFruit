53509227153b5dc9fdf08b592e09f0ff1f62f585
NSaviour <wenjian.22@163.com>
2017-09-20 11:08:50 +0800

    路演版

--- apps/tt_user/admin.py
+++ apps/tt_user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import UserInfo,UserAddressInfo
 # Register your models here.
+
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display=['pk','uname','upwd','uemail','isActive','isValid']
+
+class UserAddressAdmin(admin.ModelAdmin):
+    list_display=['pk','uname','uaddress','uphone','user']
+
+admin.site.register(UserInfo,UserInfoAdmin)
+admin.site.register(UserAddressInfo,UserAddressAdmin)
